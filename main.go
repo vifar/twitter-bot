78da664bead86fa404c8eff1b52e1d818032c06a
vifar <vifarinde@gmail.com>
2019-12-16 19:30:45 -0600

    Adding decade progress

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ import (
 	"time"
 
 	"github.com/dghubble/go-twitter/twitter"
-	"github.com/gorilla/mux"
-	newrelic "github.com/newrelic/go-agent"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -42,6 +40,7 @@ func main() {
 	// decadeBeginning := decadeEnd - 10
 
 	calcYearCompleted(time.Now().In(localTime), nextYear, client)
+	calcDecadeCompleted(time.Now().In(localTime), decadeEnd, client)
 
 	ticker := time.NewTicker(time.Minute)
 	quit := make(chan struct{})
@@ -50,7 +49,7 @@ func main() {
 			select {
 			case <-ticker.C:
 				log.Info("Checking time passed.......")
-				calcYearCompleted(time.Now().In(localTime), nextYear, client)
+				// calcYearCompleted(time.Now().In(localTime), nextYear, client)
 				calcDecadeCompleted(time.Now().In(localTime), decadeEnd, client)
 			case <-quit:
 				log.Info("Quiting.......")
@@ -61,18 +60,18 @@ func main() {
 	defer ticker.Stop()
 
 	// start new relic, and setting address to ping to prevent idling
-	config := newrelic.NewConfig("yeardecadeprogress", keystore.NewRelicKey)
-	app, _ := newrelic.NewApplication(config)
-	router := mux.NewRouter()
-	router.HandleFunc("/ping", getResponse).Methods("GET")
-	http.HandleFunc(newrelic.WrapHandleFunc(app, "/ping", getResponse))
+	// config := newrelic.NewConfig("yeardecadeprogress", keystore.NewRelicKey)
+	// app, _ := newrelic.NewApplication(config)
+	// router := mux.NewRouter()
+	// router.HandleFunc("/ping", getResponse).Methods("GET")
+	// http.HandleFunc(newrelic.WrapHandleFunc(app, "/ping", getResponse))
 
 	// setting port for New Relic, since they require it
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "5000"
-	}
-	http.ListenAndServe(":"+port, router)
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	port = "5000"
+	// }
+	// http.ListenAndServe(":"+port, router)
 
 	ch := make(chan os.Signal)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
@@ -81,16 +80,6 @@ func main() {
 }
 
 func sendTweet(status string, percent int, now time.Time, client *twitter.Client) {
-
-	status = fmt.Sprintf("Year %[1]d\n\n%[2]s - %[3]d%%", now.Year(), status, percent)
-
-	log.Info("Sending teweet.......")
-
-	// Send a Tweet
-	_, _, err := client.Statuses.Update(status, nil)
-	if err != nil {
-		log.Error(err)
-	}
 
 }
 
@@ -101,16 +90,14 @@ func calcYearCompleted(now time.Time, nextYear int, client *twitter.Client) {
 	difference := now.Sub(time.Date(nextYear, time.January, 1, 0, 0, 0, 0, time.UTC))
 
 	var percent int
-	if (now.Year() % 4) == 0 {
+	if now.Year()%4 == 0 {
 		percent = int(((daysInLeapYear - (difference.Hours() / -24)) / daysInLeapYear) * 100)
 	}
 	percent = int(((daysInYear - (difference.Hours() / -24)) / daysInYear) * 100)
 
-	log.Info("Percent: ", percent, " & Year Progress: ", yearProgress)
-
 	if percent > yearProgress {
 
-		log.Info("Composting teweet.......")
+		log.Info("Composting year progress teweet.......")
 
 		yearProgress = percent
 		var status string
@@ -121,17 +108,68 @@ func calcYearCompleted(now time.Time, nextYear int, client *twitter.Client) {
 				status += notCompleted
 			}
 		}
-		sendTweet(status, percent, now, client)
+
+		status = fmt.Sprintf("Year of %[1]d\n\n%[2]s - %[3]d%%", now.Year(), status, percent)
+
+		log.Info("Sending year progress tweet.......")
+
+		// Send a Tweet
+		_, _, err := client.Statuses.Update(status, nil)
+		if err != nil {
+			log.Error(err)
+		}
+	}
+
+	if yearProgress == 100 {
+		yearProgress = 0
 	}
 
 }
 
 func calcDecadeCompleted(now time.Time, decadeEnd int, client *twitter.Client) {
 
-	//difference := now.Sub(time.Date(decadeEnd, time.January, 1, 0, 0, 0, 0, time.UTC))
+	log.Info("Calculating decade completed.......")
 
-	// sendTweet(client)
+	difference := now.Sub(time.Date(decadeEnd, time.January, 1, 0, 0, 0, 0, time.UTC))
 
+	numofLeapYears := 0
+	for i := (decadeEnd - 10); i < decadeEnd; i++ {
+		if i%4 == 0 {
+			numofLeapYears++
+		}
+	}
+
+	daysInDecade := (10-numofLeapYears)*daysInYear + (numofLeapYears * daysInLeapYear)
+	percent := int(float64(((float64(daysInDecade) - difference.Hours()/-24) / float64(daysInDecade)) * 100))
+
+	if int(percent) > decadeProgress {
+
+		log.Info("Composting decade progress teweet.......")
+
+		decadeProgress = percent
+		var status string
+		for i := 0; i <= (100 * .5); i = i + 4 {
+			if float64(i) <= (float64(percent) * .5) {
+				status += completed
+			} else {
+				status += notCompleted
+			}
+		}
+
+		status = fmt.Sprintf("Decade of %[1]d\n\n%[2]s - %[3]d%%", decadeEnd-10, status, percent)
+
+		log.Info("Sending year progress tweet.......")
+
+		// Send a Tweet
+		_, _, err := client.Statuses.Update(status, nil)
+		if err != nil {
+			log.Error(err)
+		}
+	}
+
+	if decadeProgress == 100 {
+		decadeProgress = 0
+	}
 }
 
 func getResponse(w http.ResponseWriter, r *http.Request) {
